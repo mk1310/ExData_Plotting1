9fbc2a50dea4337c22534018dd6ccae08da442fd
MK <mk1310@live.com>
2014-05-12 02:07:46 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 ##Import and prepare data
-data <- read.table("D:/csra/IN_PROGRESS/Exploratory Analysis/assign1/exdata-data-household_power_consumption/household_power_consumption.txt", sep=";",header=TRUE)
+data <- read.table("household_power_consumption.txt", sep=";",header=TRUE)
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 data$Time <- strptime(paste(data$Date,data$Time), "%Y-%m-%d %H:%M:%S")
 d_feb <- subset(data, data$Date <= as.Date("2007-02-02") & data$Date >= as.Date("2007-02-01"))
@@ -13,7 +13,7 @@ d_feb$Sub_metering_2 <- as.numeric(as.character(d_feb$Sub_metering_2))
 d_feb$Sub_metering_3 <- as.numeric(as.character(d_feb$Sub_metering_3))
 
 ##Plot graph 2
-png(file = "D:/csra/IN_PROGRESS/Exploratory Analysis/assign1/plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "transparent",  res = NA)
+png(file = "plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "transparent",  res = NA)
 par(mfrow=c(1,1))
 plot(d_feb$Time,d_feb$Global_active_power,type="l", xlab="", ylab="Global Active Power (kilowatts)", main="")
 dev.off()
